31f2bdd45770ccff55a573321cf72354a37d09b8
moutlaw <moutlaw@lgupluspartners.co.kr>
2021-11-16 17:15:07 +0900

    yml 설정 완성

--- subprojects/presentation/src/main/java/com/lguplus/fleta/api/outer/send/SmsController.java
+++ subprojects/presentation/src/main/java/com/lguplus/fleta/api/outer/send/SmsController.java
@@ -1,11 +1,14 @@
 package com.lguplus.fleta.api.outer.send;
 
+import com.lguplus.fleta.config.SmsAgentConfig;
 import com.lguplus.fleta.data.dto.request.SendSMSCodeRequestDto;
 import com.lguplus.fleta.data.dto.response.SuccessResponseDto;
 import com.lguplus.fleta.data.vo.SendSMSCodeVo;
 import com.lguplus.fleta.service.send.SMSService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,10 +20,13 @@ public class SmsController {
 
     private final SMSService smsService;
 
+//    @Value("${agent.no.sendtime}")
+//    private String sendTime;
+
     @PostMapping("/mims/sendSms")
     public SuccessResponseDto setPayment(@Valid SendSMSCodeVo request) {
 
-        log.debug("SmsController.setPayment() - {}:{}", "SMS발송 요청", request);
+//        log.debug("sendTime - {}", sendTime);
 
         SendSMSCodeRequestDto requestDto = request.convert();
 
