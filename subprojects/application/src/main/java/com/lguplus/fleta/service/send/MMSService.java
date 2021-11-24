e77e8f9d5d9ca6680874aaf23a26aa5cd3b11a69
sos6871 <sos6871@naver.com>
2021-11-24 15:25:51 +0900

    MMS전송처리 Yml추가

--- subprojects/application/src/main/java/com/lguplus/fleta/service/send/MMSService.java
+++ subprojects/application/src/main/java/com/lguplus/fleta/service/send/MMSService.java
@@ -2,13 +2,15 @@ package com.lguplus.fleta.service.send;
 
 import com.lguplus.fleta.data.dto.request.SendMMSRequestDto;
 import com.lguplus.fleta.data.dto.response.SuccessResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class MMSService {
 
     private final MMSAgentDomainService mmsAgentDomainService;
--- subprojects/domain/src/main/java/com/lguplus/fleta/service/send/MMSAgentDomainService.java
+++ subprojects/domain/src/main/java/com/lguplus/fleta/service/send/MMSAgentDomainService.java
@@ -1,26 +1,39 @@
 package com.lguplus.fleta.service.send;
 
+import com.lguplus.fleta.client.CallSettingApiClient;
 import com.lguplus.fleta.data.dto.request.SendMMSRequestDto;
 import com.lguplus.fleta.data.dto.response.SuccessResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @Component
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@RequestParam
 @RequiredArgsConstructor
 public class MMSAgentDomainService {
-    @Value("${mms.namespace}")
-    private String mmsNamespace;
+//mmsAgentDomastervice 찾을 수없는 'com.lgeplus.fleta.client.callsettingapiclient'유형의 bean이 필요했습니다.
+    private final CallSettingApiClient aipClient;
 
     public SuccessResponseDto sendMMS(SendMMSRequestDto sendMMSRequestDto) {
-        log.debug(mmsNamespace+"<==============test");
         log.debug("MMSAgentDomainService.sendMMS() - {}:{}", "MMS발송 처리", sendMMSRequestDto);
         log.debug("MMSAgentDomainService.sendMMS() - {}:{}", "//setting API 호출하여 메세지 등록");
-        //Map<String,String> redisCcallSettingApi();
+
+        Map<String, String> prm = new HashMap<>();
+        prm.put("sa_id","mms");
+        prm.put("stb_mac","mms");
+        prm.put("code_id","");
+        prm.put("svc_type","E");
+       Map<String, Object> result = aipClient.callSettingApi(prm);
         return SuccessResponseDto.builder().build();
     }
 
