e653f6cc64a7dcca8a890763a600478ca7efef8b
sos6871 <sos6871@naver.com>
2021-11-30 10:17:13 +0900

    디렉터리 정리

--- subprojects/infrastructure/src/main/java/com/lguplus/fleta/provider/rest/CallSettingFeignClient.java
+++ subprojects/infrastructure/src/main/java/com/lguplus/fleta/provider/rest/CallSettingFeignClient.java
@@ -11,16 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.Map;
 
-//@FeignClient(name = "mms-agent-rest-message", url = "http://hdtv.suxm.uplus.co.kr")
-
 @FeignClient(name = "mms-agent-rest-message", url = "${mms.setting.rest_url}")
 public interface CallSettingFeignClient {
     /**
      *
      * @return
      */
-    @RequestMapping(method = RequestMethod.GET, value = "/hdtv/comm/setting", produces = "application/json")
+    @RequestMapping(method = RequestMethod.GET, value = "${mms.setting.rest_path}", produces = "application/json")
     public Map<String, Object> callSettingApi(@RequestParam Map<String, String> prm);
-
 
 }
