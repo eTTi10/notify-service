04bd9906a2bd6f88ab777a3bda1c178c7e9c681a
msjp7878 <msjp7878@lgupluspartners.co.kr>
2021-12-06 16:13:20 +0900

    * 메시지 코듸화 * 단건푸시 MapStruct Mapper 추가

---
+++ subprojects/presentation/src/main/java/com/lguplus/fleta/data/mapper/HttpPushSingleMapper.java
@@ -0,0 +1,19 @@
+package com.lguplus.fleta.data.mapper;
+
+import com.lguplus.fleta.config.ObjectMapperConfig;
+import com.lguplus.fleta.data.dto.request.inner.HttpPushSingleRequestDto;
+import com.lguplus.fleta.data.vo.HttpPushSingleRequestVo;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+/**
+ * 단건푸시등록 요청 MapStruct Mapper
+ *
+ */
+@Mapper(config = ObjectMapperConfig.class)
+public interface HttpPushSingleMapper {
+
+    @Mapping(target = "pushType", expression = "java(httpPushSingleRequestVo.getPushType().toUpperCase())")
+    HttpPushSingleRequestDto toDto(HttpPushSingleRequestVo httpPushSingleRequestVo);
+
+}
