9b3338d4ffeee7dfa6c4ef553755f1847227e58a
sos6871 <sos6871@naver.com>
2021-12-15 17:50:35 +0900

    * Junit작업

--- subprojects/presentation/src/test/java/com/lguplus/fleta/api/outer/latest/LatestControllerTest.java
+++ subprojects/presentation/src/test/java/com/lguplus/fleta/api/outer/latest/LatestControllerTest.java
@@ -3,15 +3,10 @@ package com.lguplus.fleta.api.outer.latest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lguplus.fleta.config.ArgumentResolverConfig;
 import com.lguplus.fleta.config.MessageConverterConfig;
-import com.lguplus.fleta.data.dto.LatestDto;
-import com.lguplus.fleta.data.dto.response.GenericRecordsetResponseDto;
-import com.lguplus.fleta.data.entity.LatestEntity;
 import com.lguplus.fleta.data.mapper.LatestPostRequestMapper;
 import com.lguplus.fleta.data.mapper.LatestSearchRequestMapper;
 import com.lguplus.fleta.service.latest.LatestService;
-import com.lguplus.fleta.util.JunitTestUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,16 +19,10 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -59,45 +48,6 @@ class LatestControllerTest {
 
     @MockBean
     private LatestPostRequestMapper latestPostRequestMapper;
-
-
-    @BeforeEach
-    void getLatestListBefore() throws Exception {
-
-        LatestEntity rs1 = new LatestEntity();
-        JunitTestUtils.setValue(rs1, "saId", "500058151453");
-        JunitTestUtils.setValue(rs1, "mac", "001c.627e.039c");
-        JunitTestUtils.setValue(rs1, "ctn", "01055805424");
-        JunitTestUtils.setValue(rs1, "catId", "T3021");
-        JunitTestUtils.setValue(rs1, "regId", "500058151453");
-        JunitTestUtils.setValue(rs1, "catName", "놀라운 대회 스타킹");
-        JunitTestUtils.setValue(rs1, "rDate", "2014-11-09 13:18:14.000");
-        JunitTestUtils.setValue(rs1, "categoryGb", "");
-
-        List<LatestEntity> rs = List.of(rs1);
-        List<LatestDto> resultList = new ArrayList<LatestDto>();
-
-        rs.forEach(e -> {
-            LatestDto item = LatestDto.builder()
-                    .saId(e.getSaId())
-                    .mac(e.getMac())
-                    .ctn(e.getCtn())
-                    .catId(e.getCatId())
-                    .catName(e.getCatName())
-                    .rDate(e.getRDate())
-                    .categoryGb(e.getCategoryGb())
-                    .build();
-            resultList.add(item);
-        });
-
-        GenericRecordsetResponseDto<LatestDto> result = GenericRecordsetResponseDto.<LatestDto>genericRecordsetResponseBuilder()
-                .totalCount(resultList.size())
-                .recordset(resultList)
-                .build();
-
-        given(latestService.getLatestList(any())).willReturn(result);
-
-    }
 
 
     //####################### Start 알림등록 ######################
