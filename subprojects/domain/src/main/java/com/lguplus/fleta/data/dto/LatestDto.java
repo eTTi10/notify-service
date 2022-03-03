29ea79c260f52db20d83c8ffe0cd39d3ae8c760b
sos6871 <msos6871@lgupluspartners.co.kr>
2022-03-03 13:50:53 +0900

    asis&tobe result 씽크작업

--- subprojects/domain/src/main/java/com/lguplus/fleta/data/dto/LatestDto.java
+++ subprojects/domain/src/main/java/com/lguplus/fleta/data/dto/LatestDto.java
@@ -1,10 +1,11 @@
 package com.lguplus.fleta.data.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lguplus.fleta.data.dto.response.CommonResponseDto;
 import io.swagger.annotations.ApiModel;
 import lombok.*;
-import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -14,16 +15,37 @@ import java.util.Date;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class LatestDto implements PlainTextibleDto, Serializable {
+
+    @JsonIgnore
+    private String saId;
+
+    @JsonIgnore
+    private String mac;
+
+    @JsonIgnore
+    private String ctn;
+
+    @JsonInclude(JsonInclude.Include.ALWAYS)
     @JsonProperty("categoryID") //카테고리 아이디
     private String catId;
 
+    @JsonIgnore
+    private String regId;
+
+    @JsonInclude(JsonInclude.Include.ALWAYS)
     @JsonProperty("categoryName") //카테고리명
     private String catName;
 
+    @JsonIgnore
+    private Date rDate;
+
+    @JsonIgnore
+    private String categoryGb;
+
     @Override
     public String toPlainText() {
-        return String.join(CommonResponseDto.Separator.COLUMN
-                , getCatId(), getCatName());
+        return String.join(CommonResponseDto.Separator.COLUMN, getCatId(), getCatName());
     }
 }
