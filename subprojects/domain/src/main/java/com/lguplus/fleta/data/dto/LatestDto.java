0d0659993d6bfa0665452a8e774cc66a837c16d3
sos6871 <msos6871@lgupluspartners.co.kr>
2022-03-03 13:00:24 +0900

    asis&tobe result 씽크작업

--- subprojects/domain/src/main/java/com/lguplus/fleta/data/dto/LatestDto.java
+++ subprojects/domain/src/main/java/com/lguplus/fleta/data/dto/LatestDto.java
@@ -15,36 +15,15 @@ import java.util.Date;
 @Builder
 @Getter
 public class LatestDto implements PlainTextibleDto, Serializable {
-
-    @JsonProperty("sa_id")  //가입자 번호
-    private String saId;
-
-    @JsonProperty("mac") //가입자 맥주소
-    private String mac;
-
-    @JsonProperty("ctn") //전화번호 or 단말 맥 어드레스
-    private String ctn;
-
-    @JsonProperty("cat_id") //카테고리 아이디
+    @JsonProperty("categoryID") //카테고리 아이디
     private String catId;
 
-    @JsonProperty("reg_id") //Push 할 Reg ID
-    private String regId;
-
-    @JsonProperty("cat_name") //카테고리명
+    @JsonProperty("categoryName") //카테고리명
     private String catName;
-
-    @JsonProperty("r_date") //등록일시
-    private Date rDate;
-
-    @JsonProperty("category_gb") //카테고리 구분
-    private String categoryGb;
 
     @Override
     public String toPlainText() {
         return String.join(CommonResponseDto.Separator.COLUMN
-                , getSaId(), getMac(), getCtn()
-                , getRegId(), getCatId(), getCatName()
-                , getRDate().toString(), getCategoryGb());
+                , getCatId(), getCatName());
     }
 }
