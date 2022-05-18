5a51ea093800e4921d0b8b462ec2898af6159828
김민석(메가존) <mzmskim@lgupluspartners.co.kr>
2022-05-18 17:51:58 +0900

    application/json String 변경

--- subprojects/infrastructure/src/main/java/com/lguplus/fleta/provider/rest/SubscriberFeignClient.java
+++ subprojects/infrastructure/src/main/java/com/lguplus/fleta/provider/rest/SubscriberFeignClient.java
@@ -1,18 +1,17 @@
 package com.lguplus.fleta.provider.rest;
 
-
 import com.lguplus.fleta.data.dto.SaIdDto;
 import com.lguplus.fleta.data.dto.response.inner.InnerResponseDto;
+import java.util.List;
+import java.util.Map;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-import java.util.Map;
-
-@FeignClient(name="subscriber", url="${service.subscriber.url}")
+@FeignClient(name = "subscriber", url = "${service.subscriber.url}")
 public interface SubscriberFeignClient {
 
-    @GetMapping(value="/subscriber/subscriberByCtn", produces = "application/json", consumes = "application/json")
+    @GetMapping(value = "/subscriber/subscriberByCtn", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     InnerResponseDto<List<SaIdDto>> getRegistrationIDbyCtn(@RequestParam Map<String, String> conditions);
 }
