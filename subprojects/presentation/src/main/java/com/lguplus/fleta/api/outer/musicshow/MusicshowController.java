6427dc244c226ef3ed689a43b06f176a3f083db8
Dohye-Kim <dhk10@lguplus.co.kr>
2022-07-05 11:25:15 +0900

    controller test

--- subprojects/presentation/src/main/java/com/lguplus/fleta/api/outer/musicshow/MusicshowController.java
+++ subprojects/presentation/src/main/java/com/lguplus/fleta/api/outer/musicshow/MusicshowController.java
@@ -45,29 +45,53 @@ public class MusicshowController {
         return responseDto;
     }
 
-    @ApiOperation(value = "알람 여부 등록")
-    @ApiImplicitParams(value = {
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "sa_id",    value = "순번: 1<br>자리수: 12<br>설명:가입번호", example = "M15030600001"),
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "stb_mac",  value = "순번: 2<br>자리수: 20<br>설명: 가입자 STB MAC Address", example="v150.3060.0001"),
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "category_id", value = "순번: 3<br>자리수: 5<br>설명: 카테고리ID", example=""),
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "album_id", value = "순번: 4<br>자리수: 15<br>설명: 앨범ID", example=""),
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "album_nm", value = "순번: 5<br>자리수: <br>설명: 앨범명", example=""),
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "start_dt", value = "순번: 6<br >자리수: 12<br>설명: 공연시작일시", example="201912012359")
-    })
-    @PostMapping("/musicshow/push")
-    public String registerPush(){
-        return null;
-    }
-
-    @ApiOperation(value = "알람 여부 삭제")
-    @ApiImplicitParams(value = {
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "sa_id",    value = "순번: 1<br>자리수: 12<br>설명:가입번호", example = "M15030600001"),
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "stb_mac",  value = "순번: 2<br>자리수: 20<br>설명: 가입자 STB MAC Address", example="v150.3060.0001"),
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "category_id", value = "순번: 3<br>자리수: 5<br>설명: 카테고리ID", example=""),
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "album_id", value = "순번: 4<br>자리수: 15<br>설명: 앨범ID", example=""),
-    })
-    @DeleteMapping("/musicshow/push")
-    public String deletePush(){
-        return null;
-    }
+//    @ApiOperation(value = "알람 여부 등록")
+//    @ApiImplicitParams(value = {
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "sa_id",    value = "순번: 1<br>자리수: 12<br>설명:가입번호", example = "M15030600001"),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "stb_mac",  value = "순번: 2<br>자리수: 20<br>설명: 가입자 STB MAC Address", example="v150.3060.0001"),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "category_id", value = "순번: 3<br>자리수: 5<br>설명: 카테고리ID", example=""),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "album_id", value = "순번: 4<br>자리수: 15<br>설명: 앨범ID", example=""),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "album_nm", value = "순번: 5<br>자리수: <br>설명: 앨범명", example=""),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "start_dt", value = "순번: 6<br >자리수: 12<br>설명: 공연시작일시", example="201912012359")
+//    })
+//    @PostMapping("/musicshow/push")
+//    public String registerPush(){
+//        return null;
+//    }
+//
+//    @ApiOperation(value = "알람 여부 삭제")
+//    @ApiImplicitParams(value = {
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "sa_id",    value = "순번: 1<br>자리수: 12<br>설명:가입번호", example = "M15030600001"),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "stb_mac",  value = "순번: 2<br>자리수: 20<br>설명: 가입자 STB MAC Address", example="v150.3060.0001"),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "category_id", value = "순번: 3<br>자리수: 5<br>설명: 카테고리ID", example=""),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "album_id", value = "순번: 4<br>자리수: 15<br>설명: 앨범ID", example=""),
+//    })
+//    @DeleteMapping("/musicshow/push")
+//    public String deletePush(){
+//        return null;
+//    }@ApiOperation(value = "알람 여부 등록")
+//    @ApiImplicitParams(value = {
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "sa_id",    value = "순번: 1<br>자리수: 12<br>설명:가입번호", example = "M15030600001"),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "stb_mac",  value = "순번: 2<br>자리수: 20<br>설명: 가입자 STB MAC Address", example="v150.3060.0001"),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "category_id", value = "순번: 3<br>자리수: 5<br>설명: 카테고리ID", example=""),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "album_id", value = "순번: 4<br>자리수: 15<br>설명: 앨범ID", example=""),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "album_nm", value = "순번: 5<br>자리수: <br>설명: 앨범명", example=""),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "start_dt", value = "순번: 6<br >자리수: 12<br>설명: 공연시작일시", example="201912012359")
+//    })
+//    @PostMapping("/musicshow/push")
+//    public String registerPush(){
+//        return null;
+//    }
+//
+//    @ApiOperation(value = "알람 여부 삭제")
+//    @ApiImplicitParams(value = {
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "sa_id",    value = "순번: 1<br>자리수: 12<br>설명:가입번호", example = "M15030600001"),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "stb_mac",  value = "순번: 2<br>자리수: 20<br>설명: 가입자 STB MAC Address", example="v150.3060.0001"),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "category_id", value = "순번: 3<br>자리수: 5<br>설명: 카테고리ID", example=""),
+//        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "album_id", value = "순번: 4<br>자리수: 15<br>설명: 앨범ID", example=""),
+//    })
+//    @DeleteMapping("/musicshow/push")
+//    public String deletePush(){
+//        return null;
+//    }
 }
--- subprojects/presentation/src/test/java/com/lguplus/fleta/api/outer/musicshow/MusicshowControllerTest.java
+++ subprojects/presentation/src/test/java/com/lguplus/fleta/api/outer/musicshow/MusicshowControllerTest.java
@@ -1,6 +1,8 @@
 package com.lguplus.fleta.api.outer.musicshow;
 
+import com.lguplus.fleta.data.dto.GetPushResponseDto;
 import com.lguplus.fleta.service.musicshow.MusicShowService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,12 +10,25 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
-@WebMvcTest(MusicshowController.class)
+@WebMvcTest
+@ContextConfiguration(classes = {MusicshowController.class
+   })
 class MusicshowControllerTest {
+
+    private static final String SUCCESS_CODE = "0000";
+    private static final String URL_TEMPLATE = "/musicshow/push";
 
     @Autowired
     private MockMvc mockMvc;
@@ -21,10 +36,30 @@ class MusicshowControllerTest {
     @MockBean
     private MusicShowService service;
 
-        @Test
-        @DisplayName("정상 조회 테스트")
-        void getPush() {
-        }
+    MultiValueMap<String, String> getValidQueryParams01() {
+        MultiValueMap<String, String> uriVars = new LinkedMultiValueMap<>();
+        uriVars.add("sa_id", "1000494369");
+        uriVars.add("stb_mac", "v010.0049.4369");
+        uriVars.add("album_id", "M01198F334PPV00");
+        return uriVars;
+    }
+
+    @Test
+    @DisplayName("정상 조회 테스트")
+    void getPush() throws Exception {
+
+        GetPushResponseDto responseDto = GetPushResponseDto.builder().build();
+        // Mock Method
+        given(service.getPush(any())).willReturn(responseDto);
+
+        MultiValueMap<String, String> queryParams = getValidQueryParams01();
+        MvcResult mvcResult = mockMvc.perform(get(URL_TEMPLATE).queryParams(queryParams))
+            .andExpect(status().isOk())
+            .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+        assertThat(response).contains(SUCCESS_CODE);    // 성공 코드가 있는지 확인
+    }
 
 //        @Test
 //        void registerPush() {
