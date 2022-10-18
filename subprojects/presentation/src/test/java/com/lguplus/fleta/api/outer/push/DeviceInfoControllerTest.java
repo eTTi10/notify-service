a0a4a699173f23400302c0fa0499b98ee94a3b97
김욱휘(LGUPLUS) <etti10@lguplus.co.kr>
2022-10-18 14:44:52 +0900

    Test 일관성 있게 수정 , JPA repository 파라미터 string -> DeviceInfoId.class , 사용하지 않는 vo 삭제 관련 mapper 삭제

--- subprojects/presentation/src/test/java/com/lguplus/fleta/api/outer/push/DeviceInfoControllerTest.java
+++ subprojects/presentation/src/test/java/com/lguplus/fleta/api/outer/push/DeviceInfoControllerTest.java
@@ -1,20 +1,10 @@
 package com.lguplus.fleta.api.outer.push;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.restdocs.cli.CliDocumentation.curlRequest;
-import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.restdocs.snippet.Attributes.key;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.queryParam;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,44 +12,31 @@ import com.lguplus.fleta.RestDocsConfig;
 import com.lguplus.fleta.advice.exhandler.OuterControllerAdvice;
 import com.lguplus.fleta.config.ArgumentResolverConfig;
 import com.lguplus.fleta.config.MessageConverterConfig;
-import com.lguplus.fleta.data.mapper.DeviceInfoPostRequestMapper;
 import com.lguplus.fleta.exhandler.ErrorResponseResolver;
 import com.lguplus.fleta.service.push.DeviceInfoService;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.support.StaticApplicationContext;
-import org.springframework.core.env.StandardEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.snippet.Snippet;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @ExtendWith({RestDocumentationExtension.class,MockitoExtension.class})
 @WebMvcTest
@@ -81,9 +58,6 @@ class DeviceInfoControllerTest {
     @MockBean
     private DeviceInfoService deviceInfoService;
 
-    @MockBean
-    private DeviceInfoPostRequestMapper deviceInfoPostRequestMapper;
-
     @InjectMocks
     private OuterControllerAdvice outerControllerAdvice;
     @Mock
@@ -96,14 +70,14 @@ class DeviceInfoControllerTest {
     private static final String NOTI_TYPE = "N";
     private static final String STB_MAC = "1111.2222.3333";
     @Test
-    void postDeviceInfo() throws Exception {
+    void postDeviceInfoSuccess() throws Exception {
         MultiValueMap<String , String> params = new LinkedMultiValueMap<>();
         params.add("sa_id", SA_ID);
         params.add("service_type", SERVICE_TYPE);
         params.add("agent_type", AGENT_TYPE);
         params.add("noti_type", NOTI_TYPE);
 
-        mockMvc.perform(RestDocumentationRequestBuilders.post("/mobile/hdtv/v1/push/deviceinfo")
+        MvcResult mvcResult = (MvcResult) mockMvc.perform(RestDocumentationRequestBuilders.post("/mobile/hdtv/v1/push/deviceinfo")
                 .content(
                     " {\"sa_id\" : \"500058151453\" ,"
                         + "\n\"service_type\" : \"A\" ,"
@@ -112,7 +86,18 @@ class DeviceInfoControllerTest {
                 .accept(MediaType.APPLICATION_JSON)
                 .queryParams(params)
             ).andExpect(status().isOk())
-            .andDo(document("{methodName}", simpleRequestParameterSnippet()));
+            .andDo(document("{methodName}", simpleRequestParameterSnippet())).andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        int status = response.getStatus();
+        String responseString = response.getContentAsString();
+
+        System.out.println("RESULT >> ["+responseString+"]");
+        assertThat(status).isEqualTo(200);
+        assertThat(responseString).contains("0000");
+
+        log.info("RESULT >> ["+responseString+"]");
+        log.info("DeivceInfoControllerTest.postDeviceInfoSuccess End");
     }
 
     private Snippet simpleRequestParameterSnippet() {
@@ -126,11 +111,7 @@ class DeviceInfoControllerTest {
     @DisplayName("postDeviceInfo 잘못된 파라미터") // 최종적으로는 200 9999에러 리스폰스인뎀.
     @Test
     void postDeviceInfoBadParam() throws Exception {
-//        mockMvc =  MockMvcBuilders.webAppContextSetup(webApplicationContext)
-//            .apply(documentationConfiguration(restDocumentation))
-//
-//            .setControllerAdvice(outerControllerAdvice)  // ExceptionHandler 등록
-//            .build();
+
         MultiValueMap<String , String> params = new LinkedMultiValueMap<>();
         params.add("sa_id", SA_ID);
         params.add("service_type", SERVICE_TYPE);
@@ -146,7 +127,17 @@ class DeviceInfoControllerTest {
                 .queryParams(params)
             ).andExpect(status().isOk())
            .andDo(document("{methodName}")).andReturn();
-        System.out.println("mvcResult = " + mvcResult);
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        int status = response.getStatus();
+        String responseString = response.getContentAsString();
+
+        System.out.println("RESULT >> ["+responseString+"]");
+        assertThat(status).isEqualTo(200);
+        assertThat(responseString).contains("5006");
+
+        log.info("RESULT >> ["+responseString+"]");
+        log.info("DeivceInfoControllerTest.postDeviceInfo End");
     }
     
     @Test
