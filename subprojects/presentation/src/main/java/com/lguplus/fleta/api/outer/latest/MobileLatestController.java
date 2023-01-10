17c986ca532d1277abaa9f5399dfc7722cbcb1d1
jiyeah16 <jiyeah@lguplus.co.kr>
2023-01-10 14:27:54 +0900

    최신회 알림 등록 삭제 API swagger 수정

--- subprojects/presentation/src/main/java/com/lguplus/fleta/api/outer/latest/MobileLatestController.java
+++ subprojects/presentation/src/main/java/com/lguplus/fleta/api/outer/latest/MobileLatestController.java
@@ -30,7 +30,7 @@ public class MobileLatestController {
     @ApiImplicitParams(value = {
         @ApiImplicitParam(paramType = "query", dataType = "string", required = true, name = "sa_id", value = "순번: 1<br/>자리수: 12<br/>설명: 가입자 번호", example = "500058151453"),
         @ApiImplicitParam(paramType = "query", dataType = "string", required = true, name = "stb_mac", value = "순번: 2<br/>자리수: 20<br/>설명: 맥 어드레스", example = "001c.627e.039c"),
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "ctn", value = "순번: 3<br/>자리수: 11<br/>설명: 가입자 전화번호", example = "01055805424"),
+        @ApiImplicitParam(paramType = "query", dataType = "string", required = true, name = "ctn", value = "순번: 3<br/>자리수: 11<br/>설명: 가입자 전화번호", example = "01055805424"),
         @ApiImplicitParam(paramType = "query", dataType = "string", required = true, name = "cat_id", value = "순번: 4<br/>자리수: 5<br/>설명: 카테고리 ID", example = "T3021"),
         @ApiImplicitParam(paramType = "query", dataType = "string", required = true, name = "cat_name", value = "순번: 5<br/>자리수: 200<br/>설명: 카테고리명", example = "놀라운 대회 스타킹"),
         @ApiImplicitParam(paramType = "query", dataType = "string", required = true, name = "reg_id", value = "순번: 6<br/>자리수: 64<br/>설명: PUSH할 REG ID", example = "500058151453"),
@@ -46,7 +46,7 @@ public class MobileLatestController {
     @ApiImplicitParams(value = {
         @ApiImplicitParam(paramType = "query", dataType = "string", required = true, name = "sa_id", value = "순번: 1<br/>자리수: 12<br/>설명: 가입자 번호", example = "500058151453"),
         @ApiImplicitParam(paramType = "query", dataType = "string", required = true, name = "stb_mac", value = "순번: 2<br/>자리수: 20<br/>설명: 맥 어드레스", example = "001c.627e.039c"),
-        @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "ctn", value = "순번: 3<br/>자리수: 11<br/>설명: 가입자 전화번호", example = "01055805424"),
+        @ApiImplicitParam(paramType = "query", dataType = "string", required = true, name = "ctn", value = "순번: 3<br/>자리수: 11<br/>설명: 가입자 전화번호", example = "01055805424"),
         @ApiImplicitParam(paramType = "query", dataType = "string", required = true, name = "cat_id", value = "순번: 4<br/>자리수: 5<br/>설명: 카테고리 ID", example = "T3021"),
         @ApiImplicitParam(paramType = "query", dataType = "string", required = false, name = "service_type", value = "순번: 5<br/>자리수: 1<br/>설명: 서비스 타입<br/>ex) NULL OR V : 모바일tv, R : VR", example = "V"),
     })
