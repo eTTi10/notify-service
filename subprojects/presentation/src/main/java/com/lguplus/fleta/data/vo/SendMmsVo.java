5f22ce0d6c6b58a3f7cc47abb1c3293102271a5d
김욱휘 <etti10@lguplus.co.kr>
2023-01-27 14:53:29 +0900

    전문과 다르게 나오는 vo 수정 , connection time 늘리기

--- subprojects/presentation/src/main/java/com/lguplus/fleta/data/vo/SendMmsVo.java
+++ subprojects/presentation/src/main/java/com/lguplus/fleta/data/vo/SendMmsVo.java
@@ -2,6 +2,8 @@ package com.lguplus.fleta.data.vo;
 
 import com.lguplus.fleta.data.annotation.ParamAlias;
 import com.lguplus.fleta.data.dto.request.SendMmsRequestDto;
+import com.lguplus.fleta.exception.ParameterLengthOverLimitException;
+import com.lguplus.fleta.exception.ParameterMissingException;
 import com.lguplus.fleta.exception.ParameterPhoneNumberErrorException;
 import com.lguplus.fleta.exception.ParameterTypeMismatchException;
 import com.lguplus.fleta.validation.Groups;
@@ -13,6 +15,7 @@ import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.ToString;
 
+//todo groups 규칙에 맞춰 r1, r2 이런식으로 수정
 @Getter
 @ToString
 @GroupSequence({Groups.C1.class, Groups.C2.class, Groups.C3.class, Groups.C4.class, Groups.C5.class, Groups.C6.class,
@@ -24,15 +27,15 @@ public class SendMmsVo {
     @ParamAlias("sa_id")
     @NotBlank(message = "sa_id 파라미터값이 전달이 안됨", groups = Groups.C1.class)//5008
     @Pattern(regexp = "[A-Za-z0-9]*$", message = "sa_id의 패턴이 일치하지 않습니다.", payload = ParameterTypeMismatchException.class, groups = Groups.C5.class)//5008
-    @Size(min = 8, message = "sa_id 의 길이가 8보다 작습니다.", payload = ParameterTypeMismatchException.class, groups = Groups.C9.class)//5008
-    @Size(max = 15, message = "sa_id 의 길이가 15보다 큽니다.", payload = ParameterTypeMismatchException.class, groups = Groups.C12.class)//5008
+    @Size(min = 8, message = "sa_id 의 길이가 8보다 작습니다.", payload = ParameterLengthOverLimitException.class, groups = Groups.C9.class)//5002
+    @Size(max = 15, message = "sa_id 의 길이가 10보다 큽니다.", payload = ParameterLengthOverLimitException.class, groups = Groups.C12.class)//5002 15인데 메세지만 10으로 표시
     private String saId;
 
     @ParamAlias("stb_mac")
-    @NotBlank(message = "stb_mac 파라미터값이 전달이 안됨", payload = ParameterTypeMismatchException.class, groups = Groups.C2.class)//5008
+    @NotBlank(message = "stb_mac 파라미터값이 전달이 안됨", payload = ParameterMissingException.class, groups = Groups.C2.class)// 5000으로 수정 해야함
     @Pattern(regexp = "[a-zA-Z0-9.]*$", message = "stb_mac의 패턴이 일치하지 않습니다.", payload = ParameterTypeMismatchException.class, groups = Groups.C6.class)//5008
     @Size(min = 10, message = "stb_mac 의 길이가 10보다 작습니다.", payload = ParameterTypeMismatchException.class, groups = Groups.C10.class)//5008
-    @Size(max = 20, message = "stb_mac 의 길이가 20보다 큽니다.", payload = ParameterTypeMismatchException.class, groups = Groups.C13.class)//5008
+    @Size(max = 20, message = "stb_mac 의 길이가 20보다 큽니다.", payload = ParameterLengthOverLimitException.class, groups = Groups.C13.class)//5008
     private String stbMac;
 
     @ParamAlias("ctn")
@@ -42,8 +45,8 @@ public class SendMmsVo {
     private String ctn;
 
     @ParamAlias("mms_cd")
-    @NotBlank(message = "mms_cd 파라미터값이 전달이 안됨", groups = Groups.C4.class)
-    @Pattern(regexp = "[A-Za-z0-9]*$", message = "mms_cd패턴이 일치하지 않습니다.", payload = ParameterTypeMismatchException.class, groups = Groups.C8.class)//5008
+    @NotBlank(message = "mms_cd 파라미터값이 전달이 안됨", groups = Groups.C4.class) //todo error, code 5000으로 수정
+    @Pattern(regexp = "[A-Za-z0-9]*$", message = "mms_cd의 패턴이 일치하지 않습니다.", payload = ParameterTypeMismatchException.class, groups = Groups.C8.class)//5008
     private String mmsCd;
 
     @ParamAlias("replacement")
